47b4251910b49556ec4504430dc844193afc6663
Mukhammad Karimov <brown@mysuperdispatch.com>
2019-03-18 22:33:25 +0500

    Added /home page with template

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from bocadillo import App
+from bocadillo import App, Templates
 
 app = App()
+templates = Templates(app)
 
 @app.route("/")
 async def index(req, res):
     res.text = "Hello, world!"
+
+@app.route("/home")
+async def home(req, res):
+    res.html = await templates.render("home.html", title='Hello, from Heroku!')
+
 
 if __name__ == "__main__":
     app.run()
